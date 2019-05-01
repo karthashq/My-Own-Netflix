021581e053687e7f6509257b9e84732a540749fe
Karthik <ashokviswa96@gmail.com>
2019-05-01 23:56:49 +0530

    added routing and changed home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TvseriesComponent } from './tvseries/tvseries.component';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'movies', component: MoviesListComponent },
+  { path: 'tvseries', component: TvseriesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
