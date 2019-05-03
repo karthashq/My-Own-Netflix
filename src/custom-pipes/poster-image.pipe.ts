6d7cb5dc66c32283c6df6b06882929a535d5ee7b
Karthik <ashokviswa96@gmail.com>
2019-05-03 22:33:00 +0530

    changed image urls to https and added wildcard routing

--- src/custom-pipes/poster-image.pipe.ts
+++ src/custom-pipes/poster-image.pipe.ts
@@ -5,6 +5,7 @@ import { PipeTransform, Pipe } from '@angular/core';
 export class ValidImageCheck implements PipeTransform {
     transform(value: any) {
         if (value) {
+            value = 'https' + value.slice(value.indexOf('://'));
             return value;
         } else {
             return './assets/no-image.jpg';
